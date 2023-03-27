efdcfee7accdbc869763b5b1676048c9c073a96b
Michal Stelmaszewski <michal.stelmaszewski@gmail.com>
2023-03-27 18:22:25 +0200

    add api for tasks, projects and users

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,18 @@ const PgClient = require('../client/pgclient')
 router.get('/tasks', PgClient.getTasks)
 router.get('/task/:id', PgClient.getTask)
 router.post('/task', PgClient.insertTask)
+router.put('/task', PgClient.updateTask)
+router.delete('/task', PgClient.deleteTask)
+
+router.get('/user/:id', PgClient.getUser)
+router.post('/user', PgClient.insertUser)
+router.put('/user', PgClient.updateUser)
+router.delete('/user', PgClient.deleteUser)
+
+router.get('/projects', PgClient.getProjects)
+router.get('/project/:id', PgClient.getProject)
+router.post('project/', PgClient.insertProject)
+router.put('/project', PgClient.updateProject)
+router.delete('/project', PgClient.deleteProject)
 
 module.exports = router
